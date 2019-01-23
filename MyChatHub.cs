d8ee70304b4966ac2a5e7fbfa6c2ffe0c82f65d4
Meir Kriheli <meirkr@gmail.com>
2019-01-24 01:00:48 +0200

    move to 2.1

--- MyChatHub.cs
+++ MyChatHub.cs
@@ -7,28 +7,28 @@ public class MyHub : Hub
 {
     public override Task OnConnectedAsync()
     {
-        return Clients.All.InvokeAsync("sendToAll", "__ADMIN__", $"{Context.ConnectionId} joined");
+        return Clients.All.SendAsync("sendToAll", "__ADMIN__", $"{Context.ConnectionId} joined");
     }
 
     public override Task OnDisconnectedAsync(Exception exception)
     {
-        return Clients.All.InvokeAsync("sendToAll", "__ADMIN__", $"{Context.ConnectionId} left");
+        return Clients.All.SendAsync("sendToAll", "__ADMIN__", $"{Context.ConnectionId} left");
     }
 
     public Task Send(string message)
     {
-        return Clients.All.InvokeAsync("sendToAll", "__ECHO_BACK__", message);
+        return Clients.All.SendAsync("sendToAll", "__ECHO_BACK__", message);
     }
 
     public int GetNumOfClients()
     {
-        Clients.All.InvokeAsync("DoSomething", "param1", 2);
+        Clients.All.SendAsync("DoSomething", "param1", 2);
         return 0;
     }
 
     public void SendToAll(string name, string message)
     {
-        Clients.All.InvokeAsync("sendToAll", name, message);
+        Clients.All.SendAsync("sendToAll", name, message);
     }
     
 }
