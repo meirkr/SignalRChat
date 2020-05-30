fad4a3d2d6a77556082246db5284e193c24bc849
Meir Kriheli <meir.kriheli@hp.com>
2020-05-31 01:21:14 +0300

    disable back cors

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,6 @@ namespace SignalRChat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
             services.AddSignalR();
         }
 
@@ -28,11 +27,6 @@ namespace SignalRChat
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors(p =>
-            {
-                p.AllowAnyOrigin();
-                p.AllowAnyHeader();
-            });
             app.UseRouting();
             
             app.UseEndpoints(routeBuilder =>
